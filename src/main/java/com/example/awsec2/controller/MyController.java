2a22dd5aa429cc44cbcd7b9e52f80b7b6447d179
Dev-Gopi <gopi@gmail.com>
2022-07-28 10:38:44 +0530

    test project

--- src/main/java/com/example/awsec2/controller/MyController.java
+++ src/main/java/com/example/awsec2/controller/MyController.java
@@ -1,16 +1,22 @@
 package com.example.awsec2.controller;
 
+import com.example.awsec2.AwsEc2Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class myController {
+public class MyController {
+    private final static Logger logger = LoggerFactory.getLogger(MyController.class);
     @GetMapping("/")
     public String welcome(){
+        logger.info("Test");
         return "This is Test Spring Boot Project";
     }
     @GetMapping("/call")
     public String call(){
+        logger.error("Calling...");
         return "Calling ...";
     }
 }
